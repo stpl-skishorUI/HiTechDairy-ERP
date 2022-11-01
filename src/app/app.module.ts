d0e04c465234d90e56fe406eeae335bddfb47318
stpl-skishorUI <77140184+stpl-skishorUI@users.noreply.github.com>
2022-11-01 15:57:38 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule,  MAT_DATE_LOCALE } from '@angular/material/core';
+import { PartialLayoutComponent } from './partial/partial-layout/partial-layout.component';
+import { FooterComponent } from './partial/partial-layout/footer/footer.component';
+import { HeaderComponent } from './partial/partial-layout/header/header.component';
+import { SidebarComponent } from './partial/partial-layout/sidebar/sidebar.component';
+import { MaterialModule } from './shared/angularMaterialModule/material.module';
+
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
+@NgModule({
+  declarations: [
+    AppComponent,
+    PartialLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    PerfectScrollbarModule
+  ],
+  providers: [ {
+    provide: PERFECT_SCROLLBAR_CONFIG,
+    useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/partial/partial-layout/partial-layout.routing.ts
@@ -0,0 +1,5 @@
+import { RouterModule, Routes } from '@angular/router';
+
+export const PartialLayoutRoutes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule), data: { title: 'Dashboard' } },
+];
