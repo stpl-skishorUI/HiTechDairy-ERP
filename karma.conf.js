42a957518e6ed1636586b19f3ee0f055f545d8b3
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-11-01 18:10:34 +0530

    migrated angular version to latest

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/AngularAdminTemplate'),
+      dir: require('path').join(__dirname, './coverage/HiTechDairy-ERP'),
       subdir: '.',
       reporters: [
         { type: 'html' },
