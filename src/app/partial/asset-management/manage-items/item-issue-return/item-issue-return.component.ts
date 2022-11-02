d14aa5a17368cb4d289b1b1af80d6c44bf812337
Sanket <panchmind1@gmail.com>
2022-11-02 12:04:46 +0530

    item return issue page done

--- src/app/partial/asset-management/manage-items/item-issue-return/item-issue-return.component.ts
+++ src/app/partial/asset-management/manage-items/item-issue-return/item-issue-return.component.ts
@@ -11,5 +11,35 @@ export class ItemIssueReturnComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  displayedColumns: string[] = ['position', 'name', 'ItemID', 'EmployeeName','IssueDate','ReturnDate','Action',];
+  dataSource = ELEMENT_DATA;
 }
+export interface PeriodicElement {
+  position: number;
+  name: string;
+  ItemID: any;
+  EmployeeName: string;
+  IssueDate:any;
+  ReturnDate:any;
+  Action:any;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 2, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 3, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 4, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 5, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 6, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 7, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 8, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 9, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 10, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 11, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 12, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 13, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 14, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 15, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+  {position: 16, name: 'Hydrogen', ItemID: 1.0079, EmployeeName: 'H',IssueDate:'hello',ReturnDate:'hello',Action:''},
+];
+
