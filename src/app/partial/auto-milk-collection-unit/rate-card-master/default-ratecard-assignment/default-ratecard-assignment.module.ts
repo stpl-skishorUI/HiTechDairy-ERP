ce9daa51ec05239ed836ac00d092513dc8c8871c
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-11-08 13:39:20 +0530

    created component for rate card category

--- src/app/partial/auto-milk-collection-unit/rate-card-master/default-ratecard-assignment/default-ratecard-assignment.module.ts
+++ src/app/partial/auto-milk-collection-unit/rate-card-master/default-ratecard-assignment/default-ratecard-assignment.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DefaultRatecardAssignmentRoutingModule } from './default-ratecard-assignment-routing.module';
 import { DefaultRatecardAssignmentComponent } from './default-ratecard-assignment.component';
+import { RateCardCategoryComponent } from './rate-card-category/rate-card-category.component';
 
 
 @NgModule({
   declarations: [
-    DefaultRatecardAssignmentComponent
+    DefaultRatecardAssignmentComponent,
+    RateCardCategoryComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/partial/auto-milk-collection-unit/rate-card-master/default-ratecard-assignment/rate-card-category/rate-card-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-card-category',
+  templateUrl: './rate-card-category.component.html',
+  styleUrls: ['./rate-card-category.component.scss']
+})
+export class RateCardCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
