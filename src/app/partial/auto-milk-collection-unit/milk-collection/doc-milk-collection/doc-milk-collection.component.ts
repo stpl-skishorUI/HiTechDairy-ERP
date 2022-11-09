a6a7a8237e5b9a81893d33851aa54392ed04c5f3
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-11-09 11:55:01 +0530

    Doc milk collection page designed

--- src/app/partial/auto-milk-collection-unit/milk-collection/doc-milk-collection/doc-milk-collection.component.ts
+++ src/app/partial/auto-milk-collection-unit/milk-collection/doc-milk-collection/doc-milk-collection.component.ts
@@ -11,5 +11,21 @@ export class DocMilkCollectionComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','quantity','Action'];
+  dataSource = ELEMENT_DATA;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Microsoft', weight: 9922955666, symbol: 'Micro123@gmail.com',quantity:'b',Action:''},
+  {position: 2, name: 'Wipro', weight: 7058942513, symbol: 'Wip856@gmail.com',quantity:'b',Action:''},
+  {position: 3, name: 'Wipro', weight: 7058942513, symbol: 'Wip856@gmail.com',quantity:'b',Action:''},
 
+
+];
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  Action:any;
+  quantity:string;
 }
