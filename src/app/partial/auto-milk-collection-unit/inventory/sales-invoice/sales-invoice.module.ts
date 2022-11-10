34e72a738ba828dcc64881bb07e6dbb07bc679fa
Sanket <panchmind1@gmail.com>
2022-11-10 18:18:38 +0530

    sales invoice master page design done.

--- src/app/partial/auto-milk-collection-unit/inventory/sales-invoice/sales-invoice.module.ts
+++ src/app/partial/auto-milk-collection-unit/inventory/sales-invoice/sales-invoice.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SalesInvoiceRoutingModule } from './sales-invoice-routing.module';
 import { SalesInvoiceComponent } from './sales-invoice.component';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { SalesInvoiceComponent } from './sales-invoice.component';
   ],
   imports: [
     CommonModule,
-    SalesInvoiceRoutingModule
+    SalesInvoiceRoutingModule,
+    MaterialModule
   ]
 })
 export class SalesInvoiceModule { }
