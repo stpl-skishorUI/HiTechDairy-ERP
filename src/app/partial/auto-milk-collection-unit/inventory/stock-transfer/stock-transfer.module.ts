a0d1b714ea6c82cc5ff90c073c28f91250699f08
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-11-10 11:42:18 +0530

    stock transfer page designed

--- src/app/partial/auto-milk-collection-unit/inventory/stock-transfer/stock-transfer.module.ts
+++ src/app/partial/auto-milk-collection-unit/inventory/stock-transfer/stock-transfer.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { StockTransferRoutingModule } from './stock-transfer-routing.module';
 import { StockTransferComponent } from './stock-transfer.component';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { StockTransferComponent } from './stock-transfer.component';
   ],
   imports: [
     CommonModule,
-    StockTransferRoutingModule
+    StockTransferRoutingModule,
+    MaterialModule
   ]
 })
 export class StockTransferModule { }
