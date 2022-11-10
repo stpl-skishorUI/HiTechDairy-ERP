3ba58c2c90df250b638ae9446574c4e50805d371
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-11-10 13:49:05 +0530

    debit Note page designed as well as popop added to the stock transfer

--- src/app/partial/auto-milk-collection-unit/inventory/debit-note/debit-note.component.ts
+++ src/app/partial/auto-milk-collection-unit/inventory/debit-note/debit-note.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SearchInventoryComponent } from '../search-inventory/search-inventory.component';
 
 @Component({
   selector: 'app-debit-note',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DebitNoteComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','Type','Quantity','FAT','Degree','Rate','Amount','CGST','Action'];
+  dataSource = ELEMENT_DATA;
+
+
+  Searchinventory(){
+    const dialogRef = this.dialog.open(SearchInventoryComponent,{
+      width: '500px',
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
+  }
 
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  Type:string;
+  Quantity:string;
+  FAT:string;
+  Degree:string;
+  Rate:string;
+  Amount:number;
+  Action:any
+  CGST:string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', Type:'ok',Quantity:'d',FAT:'ok',Degree:'btech',Rate:'five',Amount:50, CGST:'ok',Action:''},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', Type:'ok',Quantity:'d',FAT:'ok',Degree:'btech',Rate:'five',Amount:50, CGST:'ok',Action:''},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', Type:'ok',Quantity:'d',FAT:'ok',Degree:'btech',Rate:'five',Amount:50, CGST:'ok',Action:''},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', Type:'ok',Quantity:'d',FAT:'ok',Degree:'btech',Rate:'five',Amount:50, CGST:'ok',Action:''},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', Type:'ok',Quantity:'d',FAT:'ok',Degree:'btech',Rate:'five',Amount:50, CGST:'ok',Action:''},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', Type:'ok',Quantity:'d',FAT:'ok',Degree:'btech',Rate:'five',Amount:50, CGST:'ok',Action:''},
+];
