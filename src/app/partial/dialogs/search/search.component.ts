a3990c726109ed8e883bad151295214ee88d1238
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-11-11 16:16:36 +0530

    search inventry page design done

--- src/app/partial/dialogs/search/search.component.ts
+++ src/app/partial/dialogs/search/search.component.ts
@@ -11,5 +11,23 @@ export class SearchComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','quantity','Amount','Dispatch','Action'];
+  dataSource = ELEMENT_DATA;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'M', weight: 99, symbol: 'Mic',quantity:'b',Amount:'s',Dispatch:'ok',Action:''},
+  {position: 2, name: 'W', weight: 7, symbol: 'Wi',quantity:'b',Amount:'s',Dispatch:'ok',Action:''},
+  {position: 3, name: 'W', weight: 705, symbol: 'Wi5',quantity:'b',Amount:'s',Dispatch:'ok',Action:''},
 
+
+];
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  Action:any;
+  quantity:string;
+  Amount:string;
+  Dispatch:string;
 }
