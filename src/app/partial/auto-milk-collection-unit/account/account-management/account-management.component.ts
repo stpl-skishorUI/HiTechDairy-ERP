9a1d9f51c6de7fb331961014f8aacb20d4efac8a
Sanket <panchmind1@gmail.com>
2022-11-11 14:47:02 +0530

    account mgmt page design done.

--- src/app/partial/auto-milk-collection-unit/account/account-management/account-management.component.ts
+++ src/app/partial/auto-milk-collection-unit/account/account-management/account-management.component.ts
@@ -11,5 +11,20 @@ export class AccountManagementComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  displayedColumns: string[] = ['srno', 'particulars', 'date', 'debit','credit','remark','action'];
+  dataSource = ELEMENT_DATA;
 }
+export interface PeriodicElement {
+  srno: number;
+  particulars: string;
+  date:string;
+  debit:string;
+  credit:string;
+  remark:string;
+  action:any;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno:1,particulars:'particulars',date:'11-11-2022',debit:'debit val',credit:'credit val',remark:'Feedback',action:''},
+
+];
--- src/app/partial/auto-milk-collection-unit/account/account-management/account-management.module.ts
+++ src/app/partial/auto-milk-collection-unit/account/account-management/account-management.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AccountManagementRoutingModule } from './account-management-routing.module';
 import { AccountManagementComponent } from './account-management.component';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AccountManagementComponent } from './account-management.component';
   ],
   imports: [
     CommonModule,
-    AccountManagementRoutingModule
+    AccountManagementRoutingModule,
+    MaterialModule
   ]
 })
 export class AccountManagementModule { }
