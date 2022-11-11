07e77a69e43371bb539f58874f3a4a6fd6e615a5
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-11-11 12:27:28 +0530

    partywise material page design done

--- src/app/partial/auto-milk-collection-unit/account/partywise-material-rate/partywise-material-rate.module.ts
+++ src/app/partial/auto-milk-collection-unit/account/partywise-material-rate/partywise-material-rate.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PartywiseMaterialRateRoutingModule } from './partywise-material-rate-routing.module';
 import { PartywiseMaterialRateComponent } from './partywise-material-rate.component';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { PartywiseMaterialRateComponent } from './partywise-material-rate.compon
   ],
   imports: [
     CommonModule,
-    PartywiseMaterialRateRoutingModule
+    PartywiseMaterialRateRoutingModule,
+    MaterialModule
   ]
 })
 export class PartywiseMaterialRateModule { }
