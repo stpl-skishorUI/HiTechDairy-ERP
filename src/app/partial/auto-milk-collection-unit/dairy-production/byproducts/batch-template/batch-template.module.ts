fd5731b50d82c31f86e5c3dbf2ff45be4668b3b0
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-11-11 14:30:17 +0530

    batch template page design done

--- src/app/partial/auto-milk-collection-unit/dairy-production/byproducts/batch-template/batch-template.module.ts
+++ src/app/partial/auto-milk-collection-unit/dairy-production/byproducts/batch-template/batch-template.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { BatchTemplateRoutingModule } from './batch-template-routing.module';
 import { BatchTemplateComponent } from './batch-template.component';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { BatchTemplateComponent } from './batch-template.component';
   ],
   imports: [
     CommonModule,
-    BatchTemplateRoutingModule
+    BatchTemplateRoutingModule,
+    MaterialModule
   ]
 })
 export class BatchTemplateModule { }
