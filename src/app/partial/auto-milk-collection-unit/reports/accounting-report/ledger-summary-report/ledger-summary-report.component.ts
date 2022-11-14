fa7cbcf7122b7f021f0effd0dbf57381af6c90fc
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-11-14 14:12:48 +0530

    ledger page designed

--- src/app/partial/auto-milk-collection-unit/reports/accounting-report/ledger-summary-report/ledger-summary-report.component.ts
+++ src/app/partial/auto-milk-collection-unit/reports/accounting-report/ledger-summary-report/ledger-summary-report.component.ts
@@ -11,5 +11,24 @@ export class LedgerSummaryReportComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','commission','commissionrs','Action'];
+  dataSource = ELEMENT_DATA;
+}
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  commission:number;
+  commissionrs:number;
+ 
+  Action:any;
 
 }
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Microsoft', weight: 9922955666, symbol: 'Micro123@gmail.com',commission:50,commissionrs:25,Action:''},
+  {position: 2, name: 'Wipro', weight: 7058942513, symbol: 'Wip856@gmail.com',commission:50,commissionrs:25,Action:''},
+  {position: 3, name: 'Wipro', weight: 7058942513, symbol: 'Wip856@gmail.com',commission:50,commissionrs:25,Action:''},
+
+
+];
