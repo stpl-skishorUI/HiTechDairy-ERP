983b400fa18da1b0b030296f78c022bb0df121e7
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-11-14 14:21:20 +0530

    fodder page designed

--- src/app/partial/auto-milk-collection-unit/reports/accounting-report/fodder-summary-report/fodder-summary-report.component.ts
+++ src/app/partial/auto-milk-collection-unit/reports/accounting-report/fodder-summary-report/fodder-summary-report.component.ts
@@ -11,5 +11,24 @@ export class FodderSummaryReportComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','Debit','commission','commissionrs','Action'];
+  dataSource = ELEMENT_DATA;
+}
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  commission:number;
+  commissionrs:number;
+  Debit:string;
+  Action:any;
 
 }
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Microsoft', weight: 9922955666, symbol: 'Micro123@gmail.com',Debit:'yes',commission:50,commissionrs:25,Action:''},
+  {position: 2, name: 'Wipro', weight: 7058942513, symbol: 'Wip856@gmail.com',Debit:'yes',commission:50,commissionrs:25,Action:''},
+  {position: 3, name: 'Wipro', weight: 7058942513, symbol: 'Wip856@gmail.com',Debit:'yes',commission:50,commissionrs:25,Action:''},
+
+
+];
--- src/app/partial/auto-milk-collection-unit/reports/accounting-report/fodder-summary-report/fodder-summary-report.module.ts
+++ src/app/partial/auto-milk-collection-unit/reports/accounting-report/fodder-summary-report/fodder-summary-report.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { FodderSummaryReportRoutingModule } from './fodder-summary-report-routing.module';
 import { FodderSummaryReportComponent } from './fodder-summary-report.component';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { FodderSummaryReportComponent } from './fodder-summary-report.component'
   ],
   imports: [
     CommonModule,
-    FodderSummaryReportRoutingModule
+    FodderSummaryReportRoutingModule,
+    MaterialModule
   ]
 })
 export class FodderSummaryReportModule { }
